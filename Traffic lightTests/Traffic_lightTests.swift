192bd4e87deb3bda1ca1c39663dc123c76047242
Ilya <fekl13in@gmail.com>
2025-01-29 21:29:56 +0300

    Initial Commit

---
+++ Traffic lightTests/Traffic_lightTests.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_lightTests.swift
+//  Traffic lightTests
+//
+//  Created by Ilya Feklin on 29.01.2025.
+//
+
+import Testing
+@testable import Traffic_light
+
+struct Traffic_lightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
